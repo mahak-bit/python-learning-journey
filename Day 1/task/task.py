8bee935a80a183c786a15f5d8c92faf64849cab7
mahak jain <mahakj532@gmail.com>
2026-06-02 17:42:08 +0530

    Add files via upload

---
+++ Day 1/task/task.py
@@ -0,0 +1,6 @@
+print("Welcome to the band name generator")
+city = input("Which city you grew up in?\n ")
+pet = input("What is the name of your pet?\n ")
+print("your band name could be: " + city + " " + pet)
+
+
