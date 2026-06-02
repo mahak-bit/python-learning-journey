70120622aa320b77376b9ca22d49fba52b78399f
mahak jain <mahakj532@gmail.com>
2026-06-02 18:43:18 +0530

    Add files via upload

---
+++ Day 4/Lists/task.py
@@ -0,0 +1,4 @@
+import random
+cars = ["venue", "thar", "kia", "wagonr"]
+
+print(random.choice(cars))
---
+++ Day 4/Random Module/task.py
@@ -0,0 +1,12 @@
+import random
+#import my_module
+
+#random_integer = random.randint(1, 10)
+#print(random_integer)
+
+random_heads_or_tails = random.randint(1, 10)
+if random_heads_or_tails == 0 :
+    print("Heads")
+else :
+    print("tails")
+print(random_heads_or_tails)
